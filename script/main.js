ba2dfa1de34e42df535aef0a8e9962c51a66251f
Piotr Fryga <piotr.fryga@allegro.pl>
2014-04-18 14:31:00 +0200

    funkcje, filtry

--- script/main.js
+++ script/main.js
@@ -1,5 +1,11 @@
 var myApp = angular.module('myApp', []);
 
+myApp.filter('reverse', function() {
+	return function (text) {
+		return text.split("").reverse().join("");
+	}
+})
+
 function FirstCtrl ($scope) {
 	$scope.data = {
 		message: "def Piotr",
@@ -11,9 +17,57 @@ function FirstCtrl ($scope) {
 			message: "Andrzej",
 			testVar: "changed additional string"
 		}
+		$scope.data.secMessage = "Default text after click!";
 	};
 	$scope.reverseMessage = function (message) {
 		return message.split("").reverse().join("");
 	};
 }
+
+function SecondCtrl ($scope) {
+	$scope.cities = [
+		'Warszawa',
+		'Szczecin',
+		'Wrocław',
+		'Poznań',
+		'Gdańsk',
+		'Gdynia',
+		'Sopot',
+		'Kraków'
+	];
+	$scope.citiesJson = [
+		{
+			city: "Warszawa",
+			president: "Jan Nowak"
+		},
+		{
+			city: "Kraków",
+			president: "Adam Małysz"
+		},
+		{
+			city: "Łódź",
+			president: "Jane Ahonen"
+		},
+		{
+			city: "Wrocław",
+			president: "Jan Mateja"
+		},
+		{
+			city: "Poznań",
+			president: "Hanawald"
+		},
+		{
+			city: "Gdańsk",
+			president: "Wojciech Skupień"
+		},
+		{
+			city: "Gdynia",
+			president: "Łukasz Kruczek"
+		},
+		{
+			city: "Sopot",
+			president: "Robert Mateja"
+		}
+	];
+};
 
